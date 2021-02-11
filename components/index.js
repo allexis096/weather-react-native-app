2dd449120627e381b668d75d413be83bf66efcd3
allexis <allexissoares@gmail.com>
2021-02-11 17:51:07 -0300

    feat: components of the App

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as WeatherInfo } from './WeatherInfo';
+export { default as UnitsPicker } from './UnitsPicker';
+export { default as ReloadIcon } from './ReloadIcon';
+export { default as WeatherDetails } from './WeatherDetails';
+export { default as WeatherBox } from './WeatherBox';
